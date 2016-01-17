f5c97bbcf896460171259429aa8a07c4066ad250
Arnaud Rebts <arnaud.rebts@gmail.com>
2016-01-17 15:49:28 +0100

    feat(config): allow configuration of transmission connection details

--- src/transmission.js
+++ src/transmission.js
@@ -2,10 +2,10 @@ import Promise from 'bluebird'
 import Transmission from 'transmission'
 
 const transmission = new Transmission({
-  host: process.env.TRANSMISSION,
-  port: 9091,
-  username: 'admin',
-  password: 'admin'
+  host: process.env.TRANSMISSION_HOST || 'transmission',
+  port: process.env.TRANSMISSION_PORT || 9091,
+  username: process.env.TRANSMISSION_USERNAME || 'admin',
+  password: process.env.TRANSMISSION_PASSWORD || 'admin'
 })
 
 export default Promise.promisifyAll(transmission)
