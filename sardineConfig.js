528f8bc6755d3bea4de42b75b4a973a39f34ab3d
Arnaud Rebts <arnaud.rebts@gmail.com>
2016-01-27 15:28:17 +0100

    fix(postgres): make sure postgres is running before running migrations

--- sardineConfig.js
+++ sardineConfig.js
@@ -2,5 +2,11 @@ module.exports = {
   directory: 'migrations',
   driver: 'pg',
   tableName: 'sardine_migrations',
-  connection: process.env.DATABASE_URL
+  connection: {
+    host: process.env.POSTGRES_HOST || 'localhost',
+    port: parseInt(process.env.POSTGRES_PORT, 10) || 5432,
+    database: process.env.POSTGRES_DATABASE || 'kidibox',
+    user: process.env.POSTGRES_USER || 'kidibox',
+    password: process.env.POSTGRES_PASSWORD || 'kidibox'
+  }
 }
